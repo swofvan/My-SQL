762d23642ba4b92637ac5081b9be1d0dacb3f16d
swofvan <swofvanmhd@gmail.com>
2025-09-22 15:01:58 +0530

    Task 1 - add SQL script for mobile store database operations

---
+++ 22-09-2025/Mobile Store Task1.sql
@@ -0,0 +1,17 @@
+INSERT INTO `mobiles`(`ID`, `Brand`, `Model`, `Price`, `Stock`)
+VALUES (1,'Samsung', 'Galaxy M14', 12000, 30), (2, 'Redmi', 'Note 12', 15000, 25),
+    (3, 'Realme', 'Narzo 50', 13000, 20), (4, 'Samsung', 'Galaxy A23', 18000, 10);
+SELECT * FROM mobiles
+WHERE Price > 13000 OR Stock < 15 ;
+UPDATE mobiles
+SET Stock = Stock + 5, Price = 12500
+WHERE Model = 'Narzo 50';
+DELETE FROM mobiles
+WHERE ID = 2;
+SELECT MIN(Price) AS Lowest_price, MAX(Price) AS Highest_price
+FROM mobiles;
+SELECT SUM(Stock)
+FROM mobiles;
+SELECT * FROM mobiles
+ORDER BY Price DESC
+LIMIT 2;
