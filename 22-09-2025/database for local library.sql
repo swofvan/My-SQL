74f39c4d5818facf8caf2eedb94cf02628634d82
swofvan <swofvanmhd@gmail.com>
2025-09-29 21:51:05 +0530

    Task 8 - Day 8 database of a local library

---
+++ 22-09-2025/database for local library.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE `local_library`;
+USE `local_library`;
+
+CREATE TABLE `books` (
+       book_id int NOT null,
+       title text NOT null);
+
+CREATE TABLE `borrowers` (
+       borrower_id int NOT null,
+       name text not null,
+       book_id int null);
+
+INSERT INTO `books` (`book_id`,`title`)
+VALUES (1, 'The Alchemist'),
+       (2,'The Power of Now'),
+       (3, 'Think and Grow Rich'),
+       (4, 'Clean Code');
+
+INSERT INTO `borrowers` (`borrower_id`, `name`, `book_id`)
+VALUES (101, 'Alice', 1),
+       (102, 'Bob', 2),
+       (103, 'Charlie', NULL);
+
+SELECT * FROM books
+LEFT JOIN borrowers
+ON books.book_id = borrowers.book_id;
+
+SELECT books.title, borrowers.name
+FROM borrowers
+RIGHT JOIN books
+ON books.book_id = borrowers.book_id
+WHERE borrowers.book_id IS NOT null;
+
+SELECT books.book_id, books.title
+FROM books
+LEFT JOIN borrowers
+ON books.book_id = borrowers.book_id
+WHERE borrowers.book_id IS null;
